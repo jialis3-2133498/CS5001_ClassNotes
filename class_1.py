8e8c25a54aef847d01566f665fa0f8d67d018814
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-09-10 14:00:43 -0700

    first commit

---
+++ class_1.py
@@ -0,0 +1,10 @@
+# Course grade is based on the percentage, not curved
+import numpy as np
+x = [1, -2, 3, 4]
+x_abs = np.abs(x)
+print(x_abs)
+
+print("Please enter some text: ")
+x = input()
+print("Text entered", x)
+
