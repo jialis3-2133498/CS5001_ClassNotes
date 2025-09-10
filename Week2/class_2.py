fc7da644368652ec9671f46e04cf0b4fefbdd9c2
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-09-10 16:08:33 -0700

    Update

--- Week2/class_2.py
+++ Week2/class_2.py
@@ -11,3 +11,36 @@
 # In Java, it is "camelCase"
 
 print("Hello World")
+
+def main():
+    print("Hello CS 5001 pioneers!")
+main()
+
+# white space matters in Python
+# "Don \' paninc" = Don't panic or 'Don \" panic"
+# To print out \, we need to write it \\
+# We can concatenate strings using "+" or ","
+print("xyz " + "123")
+print("xyz", 123) # "," gives you an extra space by default
+# We can not use "+" to concatenate a string and an int, make sure they are in the same type. To avoid it, we do casting(convert int into a string)
+print("xyz " + str(123))
+# Use '''xxx''' or """xxx""" to expand strings in multiple lines
+
+print("1 + 2 =", 1 + 2) # When the python read it, it excute the math first, and then the string output.
+print("1 + 2 =", 1 + 2, ", You are right")
+addition = 1 + 2
+print(f"1 + 2 = {addition} , You are right")
+print(f"1 + 2 = {addition} , You are right. \
+      It is {1 + 2}")
+
+# \n means new line, \t means tab
+print("Here is a line\nHere is another line\nHere's a backslash \\")
+print("""Aread and population of cities
+      \tTokyo\t847 million\t220 sq miles
+      \tBeijing\t9210 million\t690 sq miles""")
+
+# In python, variables are dynamic, that means they come into existence when they are assigned to values. 
+# In Jave, variables are declaritive, that means we need to 
+
+
+
